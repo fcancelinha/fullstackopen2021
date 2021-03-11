b479d674861d0097bb7d93f18ed698b562777c42
fcancelinha <48698009+fcancelinha@users.noreply.github.com>
2021-03-11 17:51:52 +0000

    part1 exercise1.11 : unicafe step6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,42 +3,51 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-const Header = ({text}) => ( 
-  <h2> {text} </h2> 
+const Header = ({ text }) => (
+  <h2> {text} </h2>
 )
 
-const Statistic = ({text, count, symbol}) => ( 
-  <li>{text} <b>{count}</b> {symbol} </li> 
+const Statistic = ({ text, count, symbol }) => (
+  <tr>
+    <td>
+      {text}
+    </td>
+    <td>
+      <b>{count}</b> {symbol}
+    </td>
+  </tr>
 )
 
 const Statistics = (props) => {
 
-  let {good, neutral, bad} = props.args
+  let { good, neutral, bad } = props.args
 
   let total = good + neutral + bad
   let positive = good ? (good / total) * 100 : 0
   let average = (good - bad) / total
 
-  if(good || neutral || bad) {
-    return (  
-    <ul>
-      <Statistic text={"good"} count={good} />
-      <Statistic text={"neutral"} count={neutral} />
-      <Statistic text={"bad"} count={bad} />
-      <Statistic text={"all: "} count={total} />
-      <Statistic text={"average: "} count={average} />
-      <Statistic text={"positive: "} count={positive} symbol={"%"} />
-    </ul>
+  if (good || neutral || bad) {
+    return (
+      <table>
+        <tbody>
+          <Statistic text={"good"} count={good} />
+          <Statistic text={"neutral"} count={neutral} />
+          <Statistic text={"bad"} count={bad} />
+          <Statistic text={"all: "} count={total} />
+          <Statistic text={"average: "} count={average} />
+          <Statistic text={"positive: "} count={positive} symbol={"%"} />
+        </tbody> 
+      </table>
     )
-  
+
   }
-  
+
   return <p> No feedback given </p>
 }
 
 
-const Button = ({handleClick, text}) => ( 
-  <button onClick={handleClick}> {text} </button> 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}> {text} </button>
 )
 
 
@@ -54,11 +63,11 @@ const App = () => {
   const increaseBad = () => setBad(bad + 1)
 
   return (
-    
+
     <div id="main">
-      
+
       <Header text={"give feedback"} />
-      
+
       <div>
         <Button handleClick={increaseGood} text={"Good"} />
         <Button handleClick={increaseNeutral} text={"Neutral"} />
@@ -68,7 +77,7 @@ const App = () => {
       <Header text={"statistics"} />
 
       <div>
-        <Statistics args={{good, neutral, bad}} />
+        <Statistics args={{ good, neutral, bad }} />
       </div>
 
     </div>
