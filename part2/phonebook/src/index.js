f1d19f9f27c3b0f8610409578b5876d137e2a01e
fcancelinha <48698009+fcancelinha@users.noreply.github.com>
2021-03-25 19:05:29 +0000

    part2 phonebook: exercise 2.10

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react'
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
+import Filter from './components/Filter'
+import Contacts from './components/Contacts'
+import ContactForm from './components/ContactForm'
 
 
-
-const Contact = ({ info }) => {
-
-  return <li> {info.name} | tel: {info.number}</li>
-}
 
 
 const App = () => {
@@ -15,71 +13,25 @@ const App = () => {
     { id: 21, name: 'Ada Lovelace', number: '39-44-5323523' },
     { id: 33, name: 'Dan Abramov', number: '12-43-234345' },
     { id: 44, name: 'Mary Poppendieck', number: '39-23-6423122' }])
-  const [newEntry, setNewEntry] = useState({ id: 0, name: '', number: 0 })
   const [filter, setFilter] = useState("")
-
-
-  const handleInput = (key, e) => {
-
-    setNewEntry({
-      ...newEntry,
-      [key]: e.target.value
-    })
-
-    console.log(newEntry);
-  }
-
-
-
-  const submitInput = (event) => {
-
-    event.preventDefault();
-
-    if (!newEntry.name || !newEntry.number)
-      return alert("there are fields left blank")
-
-    for (const ele of persons) {
-      if (ele.name === newEntry.name)
-        return alert(`${ele.name} already exist in the phonebook`)
-
-      if (ele.number === newEntry.number)
-        return alert(`${ele.number} already exist in the phonebook`)
-    }
-
-    newEntry.id = persons.length + 1
-    setPersons(persons.concat(newEntry))
-    setNewEntry({ id: 0, name: '', number: 0 })
-  }
-
+  
+  const handlePerson = (entry) => setPersons(persons.concat(entry))
+  
 
   return (
     <div>
 
       <h2>Phonebook</h2>
 
-      <div> filter shown with: <input value={filter} onChange={(event) => setFilter(event.target.value)} /> </div>
+      <Filter filter={filter} onChange={(event) => setFilter(event.target.value)}/>
 
       <h2>add a new</h2>
 
-      <form>
-
-        <div> name: <input value={newEntry.name} onChange={(event) => handleInput('name', event)} /></div>
-        <div> number: <input value={newEntry.number || ''} onChange={(event) => handleInput('number', event)} /> </div>
-
-        <div>
-          <button type="submit" onClick={submitInput}>add</button>
-        </div>
-
-      </form>
+      <ContactForm persons={persons} personHandler={handlePerson} />
 
       <h2>Numbers</h2>
 
-      <ul>
-        {persons.filter(x => x.name.toLowerCase().includes(filter.toLowerCase())).map(c =>
-          <Contact key={c.id} info={c} />
-        )}
-      </ul>
-
+      <Contacts persons={persons} filter={filter} />
 
     </div>
   )
