795a42aeac2f872e93ec1cdd5780f7bb06bf0b58
fcancelinha <48698009+fcancelinha@users.noreply.github.com>
2021-03-25 22:37:31 +0000

    part2 phonebook: exercise 2.11

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,21 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import Filter from './components/Filter'
 import Contacts from './components/Contacts'
 import ContactForm from './components/ContactForm'
+import axios from 'axios'
 
 
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { id: 12, name: 'Arto Hellas', number: '040-123456' },
-    { id: 21, name: 'Ada Lovelace', number: '39-44-5323523' },
-    { id: 33, name: 'Dan Abramov', number: '12-43-234345' },
-    { id: 44, name: 'Mary Poppendieck', number: '39-23-6423122' }])
+  const [persons, setPersons] = useState([])
   const [filter, setFilter] = useState("")
   
   const handlePerson = (entry) => setPersons(persons.concat(entry))
+
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log("data", response)
+      setPersons(response.data)
+    })
+  }, [])
   
 
   return (
