4ddc469b9e567b55b96eabf1e13eed79743f955a
Bacardi <48698009+fcancelinha@users.noreply.github.com>
2021-04-04 23:56:56 +0100

    part4 Blogs: exercise 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -7,8 +7,18 @@ const totalLikes = (blogs) => {
     return blogs.length ? blogs.reduce((x, y) => x + y.likes, 0) : 0
 }
 
+const favouriteBlog = (blogs) => {
+
+    if(blogs && blogs.length){
+        const {_id, __v, url, ...obj} = blogs.sort((a,b) => b.likes - a.likes)[0]
+        return obj
+    }
+
+}
+
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
 }
