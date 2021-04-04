c260d7acbe1cfa204c976d10576005a92272ab1e
Bacardi <48698009+fcancelinha@users.noreply.github.com>
2021-04-04 22:43:48 +0100

    part4 Blogs: exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,9 +2,13 @@ const dummy = (blog) => {
     return 1
 }
 
-
+const totalLikes = (blogs) => {
+    
+    return blogs.length ? blogs.reduce((x, y) => x + y.likes, 0) : 0
+}
 
 
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
